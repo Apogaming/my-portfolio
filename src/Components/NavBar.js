9923215aabd72f2217779938085745b230b9dce9
Nguyen Tuan Anh <apogaming@yandex.ru>
2021-02-08 23:46:09 +0100

    fix copy right

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,28 +7,41 @@ function NavBar() {
         <div className="NavBar">
             <nav className="nav">
                 <div className="profile">
-                    <img src={avatar} atl="" />
+                    <img src={avatar} alt="" />
                 </div>
 
                 <ul className="nav-items">
                     <li className="nav-item">
-                        <NavLink to="/" exact activeClassName="active">Home</NavLink>
+                        <NavLink to="/home" exact activeClassName="active">
+                            Home
+                            </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/about" exact activeClassName="active">About</NavLink>
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                            </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/portfolios" exact activeClassName="active">Portfolios</NavLink>
+                        <NavLink to="/portfolios" exact activeClassName="active">
+                            Portfolios
+                            </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/blogs" exact activeClassName="active">Blogs</NavLink>
+                        <NavLink to="/blogs" exact activeClassName="active">
+                            Blogs
+                            </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/contact" exact activeClassName="active">Contact</NavLink>
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
+                            </NavLink>
                     </li>
                 </ul>
+
                 <footer className="footer">
-                    <p> @2021 aaaaaaaaa</p>
+                    <p>
+                        @2021 Tuan Anh Nguyen
+                        </p>
                 </footer>
             </nav>
 
